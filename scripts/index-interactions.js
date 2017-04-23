7dc18aea99b7ac3d4627c2f5e4ec37a72bb00517
Brat Wiekszy <bratwiekszy@gmail.com>
2017-04-24 00:24:44 +0200

    Added using localStorage to store meme votes. FUCKING BUG BUTTONS TOGGLED OFF ACTUALLY ARE STILL DRAWN TOGGLED. FUCK YOU BUTSTRAP

--- scripts/index-interactions.js
+++ scripts/index-interactions.js
@@ -1,23 +1,64 @@
-$(function(){
+$(function(e){
     $("div[data-toggle='buttons'] label").click(function(){
-        /*
-        var fireys = $("div div.niceys-count").children("span.badge");
-        fireys.each(function(){
-            var num = Number($(this).html());
-            console.log(num);
-            $(this).html(num+1);
-        })
-        */
         var t = $(this).children("input");
         var badge = $(this).parent().next(".niceys-count").children("span.badge");
-        if($(this).parent().attr("set") !== t.attr("name")){
-            if(t.attr("name") == "nicey"){
-                badge.html(1 + Number(badge.html()));
-            }
-            else{
-                badge.html( Number(badge.html()) - 1);
-            }
+        var memName = $(this).parent().parent().prev().attr("src");
+        var currentSet = $(this).parent().attr("set");
+        if(currentSet !== t.attr("name")){
+            if(currentSet == null)
+                incVotes(t, badge, 1);
+            else
+                incVotes(t, badge, 2);
             $(this).parent().attr("set", t.attr("name"));
+            if(typeof(Storage) !== "undefined"){
+                var mem = { name: memName, comment:"", vote: t.attr("name") };
+                localStorage.setItem(memName, JSON.stringify(mem));
+            }
+        }
+        else{
+            $("*").removeClass('active');
+            t.prop("checked", false);
+            incVotes(t, badge, -1);
+            $(this).parent().attr("set", null);
+            if(typeof(Storage) !== "undefined"){
+                localStorage.removeItem(memName);
+            }
+        }
+    });
+    $(".meme img").each(function(){
+        
+        var memName = $(this).attr("src");
+        if(typeof(Storage) !== "undefined"){
+            var memjson = localStorage.getItem(memName);
+            if(memjson != null){
+                var mem = JSON.parse(memjson);
+                var vote = mem.vote;
+                var group = $(this).next().children(".btn-group");
+                group.first().attr("set", vote);
+                var input = group.first().children("label");
+                var imp;
+                var num;
+                if(vote === "nicey"){
+                    imp = input.first();
+                    num = 1;
+                }
+                else{
+                    imp = input.last();
+                    num = -1;
+                }
+                imp.addClass("active");
+                var badge = imp.parent().next(".niceys-count").children("span.badge");
+                badge.html(num + Number(badge.html()));
+            }
         }
     })
 });
+
+function incVotes(t, badge, num){
+    if(t.attr("name") == "nicey"){
+        badge.html(num + Number(badge.html()));
+    }
+    else{
+        badge.html( Number(badge.html()) - num);
+    }
+}
